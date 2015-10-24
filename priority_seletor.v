9b5784c74be9f96540f7184e78dc7e61c66dd77b
Qian Shen <shenqian@umich.edu>
2015-10-24 19:32:18 -0400

    modified:   RS_one_entry.v 	modified:   priority_seletor.v 	modified:   rs.v

--- priority_seletor.v
+++ priority_seletor.v
@@ -28,17 +28,17 @@ endmodule
 
 
 module priority_selector ( 
-		input           [RS_SIZE-1:0] req,
+		input           [`RS_SIZE-1:0] req,
                 input           en,
-                output logic    [RS_SIZE-1:0] gnt
+                output logic    [`RS_SIZE-1:0] gnt
 			);
 
 
-    logic [RS_SIZE-1:0] [1:0]    sub_reqs;    // req_up of lower ps connects to req of higher
-    logic [RS_SIZE-1:0] [1:0]    sub_gnts;    // gnt of higher ps connects to enable of lower
+    logic [`RS_SIZE-1:0] [1:0]    sub_reqs;    // req_up of lower ps connects to req of higher
+    logic [`RS_SIZE-1:0] [1:0]    sub_gnts;    // gnt of higher ps connects to enable of lower
 
     generate
-        for(genvar i=1; i<RS_SIZE; i++) begin    // Instantiate N-1 ps2 submodules
+        for(genvar i=1; i<`RS_SIZE; i++) begin    // Instantiate N-1 ps2 submodules
             					 // the following is a local parameter defined at compile-time to distinguish left 
            					 // subtrees from right subtrees via the least significant bit of i
             localparam left_right = i[0];        // i = odd number -> left sub-tree of parent
@@ -51,10 +51,10 @@ module priority_selector (
 
     endgenerate
     
-    assign sub_reqs[ RS_SIZE-1 : RS_SIZE/2] = req;    // connect top-level requests to bottom layer selectors
+    assign sub_reqs[ `RS_SIZE-1 : `RS_SIZE/2] = req;    // connect top-level requests to bottom layer selectors
     
     assign sub_gnts[0][1] = en;           	      // connect enable to top layer selector
-    assign gnt = sub_gnts[ RS_SIZE-1 : RS_SIZE/2];    // connect bottom layer selectors to top-level grants
+    assign gnt = sub_gnts[ `RS_SIZE-1 : `RS_SIZE/2];    // connect bottom layer selectors to top-level grants
     
 endmodule
 
