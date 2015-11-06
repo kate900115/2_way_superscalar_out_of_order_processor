6711c3f5d414ea189e51c2fd211bb0e02ab6d600
Lifan Gong <golifan@umich.edu>
2015-11-05 22:32:27 -0500

    update test_ex and makefile

--- test_bench/test_rs.v
+++ test_bench/test_rs.v
@@ -43,65 +43,24 @@ module testbench_rs;
 	logic					fu2_memory_available;
   
  	//outputs
-	logic [63:0]						fu1_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu1_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu1_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu1_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]						fu1_rs_op_type_out;     // This RS' operation type
-	logic							fu1_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu1_alu_func_out;
+	logic [5:0][63:0]					fu_rs_opa_out;       	// This RS' opa 
+	logic [5:0][63:0]					fu_rs_opb_out;       	// This RS' opb 
+	logic [5:0][$clog2(`PRF_SIZE)-1:0]			fu_rs_dest_tag_out;  	// This RS' destination tag  
+	logic [5:0][$clog2(`ROB_SIZE)-1:0]    			fu_rs_rob_idx_out;   	// This RS' corresponding ROB index
+	logic [5:0][5:0]					fu_rs_op_type_out;     // This RS' operation type
+	logic [5:0]						fu_rs_out_valid;		// RS output is valid
+	ALU_FUNC [5:0]						fu_alu_func_out;
 
-	logic [63:0]						fu2_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu2_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu2_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu2_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]					  	fu2_rs_op_type_out;    // This RS' operation type
-	logic							fu2_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu2_alu_func_out;
-	logic [1:0]						rs_full;				// RS is full now
-
-	logic [63:0]						fu3_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu3_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu3_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu3_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]						fu3_rs_op_type_out;     // This RS' operation type
-	logic							fu3_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu3_alu_func_out;
-
-	logic [63:0]						fu4_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu4_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu4_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu4_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]						fu4_rs_op_type_out;     // This RS' operation type
-	logic							fu4_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu4_alu_func_out;
-
-	logic [63:0]						fu5_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu5_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu5_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu5_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]						fu5_rs_op_type_out;     // This RS' operation type
-	logic							fu5_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu5_alu_func_out;
-
-	logic [63:0]						fu6_rs_opa_out;       	// This RS' opa 
-	logic [63:0]						fu6_rs_opb_out;       	// This RS' opb 
-	logic [$clog2(`PRF_SIZE)-1:0]				fu6_rs_dest_tag_out;  	// This RS' destination tag  
-	logic [$clog2(`ROB_SIZE)-1:0]    			fu6_rs_rob_idx_out;   	// This RS' corresponding ROB index
-	logic [5:0]						fu6_rs_op_type_out;     // This RS' operation type
-	logic							fu6_rs_out_valid;		// RS output is valid
-	ALU_FUNC                                                fu6_alu_func_out;
-	
 	rs DUT(.reset(reset),
-	   .clock(clock),           
+	   .clock(clock),
 	   .inst1_rs_dest_in(inst1_rs_dest_in),
 	   .inst2_rs_dest_in(inst2_rs_dest_in),
-   
-	   .rs_cdb1_in(rs_cdb1_in),      
-	   .rs_cdb1_tag(rs_cdb1_tag),    
-	   .rs_cdb1_valid(rs_cdb1_valid),  
-	   .rs_cdb2_in(rs_cdb2_in),     
-	   .rs_cdb2_tag(rs_cdb2_tag),   
+
+	   .rs_cdb1_in(rs_cdb1_in),
+	   .rs_cdb1_tag(rs_cdb1_tag),
+	   .rs_cdb1_valid(rs_cdb1_valid),
+	   .rs_cdb2_in(rs_cdb2_in),
+	   .rs_cdb2_tag(rs_cdb2_tag),
 	   .rs_cdb2_valid(rs_cdb2_valid),
    
 	   .inst1_rs_opa_in(inst1_rs_opa_in),
@@ -131,48 +90,13 @@ module testbench_rs;
 	   .fu2_memory_available(fu2_memory_available),
   
 
-	   .fu1_rs_opa_out(fu1_rs_opa_out),       	// This RS' opa 
-	   .fu1_rs_opb_out(fu1_rs_opb_out),       	// This RS' opb 
-	   .fu1_rs_dest_tag_out(fu1_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu1_rs_rob_idx_out(fu1_rs_rob_idx_out),   	// 
-	   .fu1_rs_op_type_out(fu1_rs_op_type_out),     //
-	   .fu1_rs_out_valid(fu1_rs_out_valid),
-	   .fu1_alu_func_out(fu1_alu_func_out),
-	   .fu2_rs_opa_out(fu2_rs_opa_out),       	// This RS' opa 
-	   .fu2_rs_opb_out(fu2_rs_opb_out),       	// This RS' opb 
-	   .fu2_rs_dest_tag_out(fu2_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu2_rs_rob_idx_out(fu2_rs_rob_idx_out),   	// 
-	   .fu2_rs_op_type_out(fu2_rs_op_type_out),     //
-	   .fu2_rs_out_valid(fu2_rs_out_valid),
-	   .fu2_alu_func_out(fu2_alu_func_out),
-	   .fu3_rs_opa_out(fu3_rs_opa_out),       	// This RS' opa 
-	   .fu3_rs_opb_out(fu3_rs_opb_out),       	// This RS' opb 
-	   .fu3_rs_dest_tag_out(fu3_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu3_rs_rob_idx_out(fu3_rs_rob_idx_out),   	// 
-	   .fu3_rs_op_type_out(fu3_rs_op_type_out),     //
-	   .fu3_rs_out_valid(fu3_rs_out_valid),
-	   .fu3_alu_func_out(fu3_alu_func_out),
-	   .fu4_rs_opa_out(fu4_rs_opa_out),       	// This RS' opa 
-	   .fu4_rs_opb_out(fu4_rs_opb_out),       	// This RS' opb 
-	   .fu4_rs_dest_tag_out(fu4_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu4_rs_rob_idx_out(fu4_rs_rob_idx_out),   	// 
-	   .fu4_rs_op_type_out(fu4_rs_op_type_out),     //
-	   .fu4_rs_out_valid(fu4_rs_out_valid),
-	   .fu4_alu_func_out(fu4_alu_func_out),
-	   .fu5_rs_opa_out(fu5_rs_opa_out),       	// This RS' opa 
-	   .fu5_rs_opb_out(fu5_rs_opb_out),       	// This RS' opb 
-	   .fu5_rs_dest_tag_out(fu5_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu5_rs_rob_idx_out(fu5_rs_rob_idx_out),   	// 
-	   .fu5_rs_op_type_out(fu5_rs_op_type_out),     //
-	   .fu5_rs_out_valid(fu5_rs_out_valid),
-	   .fu5_alu_func_out(fu5_alu_func_out),
-	   .fu6_rs_opa_out(fu6_rs_opa_out),       	// This RS' opa 
-	   .fu6_rs_opb_out(fu6_rs_opb_out),       	// This RS' opb 
-	   .fu6_rs_dest_tag_out(fu6_rs_dest_tag_out),  	// This RS' destination tag  
-	   .fu6_rs_rob_idx_out(fu6_rs_rob_idx_out),   	// 
-	   .fu6_rs_op_type_out(fu6_rs_op_type_out),     //
-	   .fu6_rs_out_valid(fu6_rs_out_valid),
-	   .fu6_alu_func_out(fu6_alu_func_out),
+	   .fu_rs_opa_out(fu_rs_opa_out),
+	   .fu_rs_opb_out(fu_rs_opb_out),
+	   .fu_rs_dest_tag_out(fu_rs_dest_tag_out),
+	   .fu_rs_rob_idx_out(fu_rs_rob_idx_out),
+	   .fu_rs_op_type_out(fu_rs_op_type_out),
+	   .fu_rs_out_valid(fu_rs_out_valid),
+	   .fu_alu_func_out(fu_alu_func_out),
 
 	   .rs_full(rs_full)
 	);
@@ -194,12 +118,12 @@ module testbench_rs;
 	        fu4_rs_dest_tag_out:%d, fu4_rs_rob_idx_out:%d, fu4_rs_op_type_out:%d, fu4_alu_func_out:%d, fu4_rs_opa_out:%d, fu4_rs_opb_out:%d, fu4_rs_out_valid:%b, \n \
 	        fu5_rs_dest_tag_out:%d, fu5_rs_rob_idx_out:%d, fu5_rs_op_type_out:%d, fu5_alu_func_out:%d, fu5_rs_opa_out:%d, fu5_rs_opb_out:%d, fu5_rs_out_valid:%b, \n \
 	        fu6_rs_dest_tag_out:%d, fu6_rs_rob_idx_out:%d, fu6_rs_op_type_out:%d, fu6_alu_func_out:%d, fu6_rs_opa_out:%d, fu6_rs_opb_out:%d, fu6_rs_out_valid:%b, rs_full: %b",
-				$time, clock, fu1_rs_dest_tag_out, fu1_rs_rob_idx_out, fu1_rs_op_type_out, fu1_alu_func_out, fu1_rs_opa_out, fu1_rs_opb_out, fu1_rs_out_valid, 
-					      fu2_rs_dest_tag_out, fu2_rs_rob_idx_out, fu2_rs_op_type_out, fu2_alu_func_out, fu2_rs_opa_out, fu2_rs_opb_out, fu2_rs_out_valid, 
-					      fu3_rs_dest_tag_out, fu3_rs_rob_idx_out, fu3_rs_op_type_out, fu3_alu_func_out, fu3_rs_opa_out, fu3_rs_opb_out, fu3_rs_out_valid,
-					      fu4_rs_dest_tag_out, fu4_rs_rob_idx_out, fu4_rs_op_type_out, fu4_alu_func_out, fu4_rs_opa_out, fu4_rs_opb_out, fu4_rs_out_valid,
-					      fu5_rs_dest_tag_out, fu5_rs_rob_idx_out, fu5_rs_op_type_out, fu5_alu_func_out, fu5_rs_opa_out, fu5_rs_opb_out, fu5_rs_out_valid,
-					      fu6_rs_dest_tag_out, fu6_rs_rob_idx_out, fu6_rs_op_type_out, fu6_alu_func_out, fu6_rs_opa_out, fu6_rs_opb_out, fu6_rs_out_valid,rs_full);
+				$time, clock, fu_rs_dest_tag_out[0], fu_rs_rob_idx_out[0], fu_rs_op_type_out[0], fu_alu_func_out[0], fu_rs_opa_out[0], fu_rs_opb_out[0], fu_rs_out_valid[0], 
+					      fu_rs_dest_tag_out[1], fu_rs_rob_idx_out[1], fu_rs_op_type_out[1], fu_alu_func_out[1], fu_rs_opa_out[1], fu_rs_opb_out[1], fu_rs_out_valid[1], 
+					      fu_rs_dest_tag_out[2], fu_rs_rob_idx_out[2], fu_rs_op_type_out[2], fu_alu_func_out[2], fu_rs_opa_out[2], fu_rs_opb_out[2], fu_rs_out_valid[2],
+					      fu_rs_dest_tag_out[3], fu_rs_rob_idx_out[3], fu_rs_op_type_out[3], fu_alu_func_out[3], fu_rs_opa_out[3], fu_rs_opb_out[3], fu_rs_out_valid[3],
+					      fu_rs_dest_tag_out[4], fu_rs_rob_idx_out[4], fu_rs_op_type_out[4], fu_alu_func_out[4], fu_rs_opa_out[4], fu_rs_opb_out[4], fu_rs_out_valid[4],
+					      fu_rs_dest_tag_out[5], fu_rs_rob_idx_out[5], fu_rs_op_type_out[5], fu_alu_func_out[5], fu_rs_opa_out[5], fu_rs_opb_out[5], fu_rs_out_valid[5],rs_full);
 		clock = 0;
 		//***RESET**
 		reset = 1;
@@ -210,7 +134,7 @@ module testbench_rs;
 		inst1_rs_opa_in		= 0;
 		inst1_rs_opb_in		= 8;
 		inst1_rs_opa_valid	= 1;
-		inst1_rs_opb_valid	= 0;
+		inst1_rs_opb_valid	= 1;
 		inst1_rs_op_type_in	= `LDA_INST;
 		inst1_rs_alu_func	= ALU_ADDQ;
 		inst1_rs_load_in	= 1;
@@ -231,19 +155,13 @@ module testbench_rs;
 		fu2_mult_available	= 1;
 		fu2_adder_available	= 1;
 		fu2_memory_available	= 1;
-
-		rs_cdb1_valid	= 0;
-		rs_cdb2_valid	= 1;
-		rs_cdb2_in	= 9;
-		rs_cdb2_tag	= 8;
-		
 		#5
 
 		@(negedge clock);
 		inst1_rs_dest_in	= 3;
 		inst1_rs_opa_in		= 2;
 		inst1_rs_opb_in		= 16;
-		inst1_rs_opa_valid	= 0;
+		inst1_rs_opa_valid	= 1;
 		inst1_rs_opb_valid	= 1;
 		inst1_rs_op_type_in	= `SLL_INST;
 		inst1_rs_alu_func	= ALU_SLL;
@@ -254,7 +172,7 @@ module testbench_rs;
 		fu1_memory_available	= 1;
 		
 		inst2_rs_dest_in	= 4;
-		inst2_rs_opa_in		= 0;
+		inst2_rs_opa_in		= 1;
 		inst2_rs_opb_in		= 64'h2ee6;
 		inst2_rs_opa_valid	= 1;
 		inst2_rs_opb_valid	= 1;
@@ -265,19 +183,14 @@ module testbench_rs;
 		fu2_mult_available	= 1;
 		fu2_adder_available	= 1;
 		fu2_memory_available	= 1;
-
-		rs_cdb1_valid	= 0;
-		rs_cdb2_valid	= 0;
-		rs_cdb2_in	= 9;
-		rs_cdb2_tag	= 8;
 		#5
 
 		@(negedge clock);
 		inst1_rs_dest_in	= 5;
 		inst1_rs_opa_in		= 4;
 		inst1_rs_opb_in		= 3;
-		inst1_rs_opa_valid	= 0;
-		inst1_rs_opb_valid	= 0;
+		inst1_rs_opa_valid	= 1;
+		inst1_rs_opb_valid	= 1;
 		inst1_rs_op_type_in	= `BIS_INST;
 		inst1_rs_alu_func	= ALU_BIS;
 		inst1_rs_load_in	= 1;
@@ -287,7 +200,7 @@ module testbench_rs;
 		fu1_memory_available	= 1;
 		
 		inst2_rs_dest_in	= 6;
-		inst2_rs_opa_in		= 0;
+		inst2_rs_opa_in		= 1;
 		inst2_rs_opb_in		= 64'h876;
 		inst2_rs_opa_valid	= 1;
 		inst2_rs_opb_valid	= 1;
@@ -298,10 +211,8 @@ module testbench_rs;
 		fu2_mult_available	= 1;
 		fu2_adder_available	= 1;
 		fu2_memory_available	= 1;
-
-		rs_cdb1_valid=0;
-		rs_cdb2_valid=0;
 		#5
+		@(negedge clock);
 		$display("@@@Passed");
 		$finish;
 	end
