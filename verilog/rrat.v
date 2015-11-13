2025874ebb108d70ab30d89fc3441fe55325fb89
gaoying <gaoying@umich.edu>
2015-11-12 20:32:09 -0500

    new rrat.v

--- verilog/rrat.v
+++ verilog/rrat.v
@@ -60,25 +60,43 @@ end
 always_comb begin
 	if(mispredict_sig1 & inst1_enable)
 	  		mispredict_up_idx = rrat_reg;
-		else if(mispredict_sig2 & inst2_enable) begin
+	else if(mispredict_sig2 & inst2_enable) begin
 			for(i=0; i<`ARF_SIZE; i++) begin
 				if(i==RoB_ARF_idx1)
 				mispredict_up_idx[i] = RoB_PRF_idx1;
 				else
 				mispredict_up_idx[i] = rrat_reg[i];
 			end //for
-		end //else if
-		else
+	end //else if
+	else 
 		mispredict_up_idx = 0;
 
-		PRF_free_valid1 	= (RoB_retire_in1 & inst1_enable) ? 1 : 0;
-		PRF_free_idx1 	= (RoB_retire_in1 & inst1_enable) ? rrat_reg[RoB_ARF_idx1] : 0;
-		n_rrat_reg[RoB_ARF_idx1]= (RoB_retire_in1 & inst1_enable) ? RoB_PRF_idx1 : rrat_reg[RoB_ARF_idx1];	
+	PRF_free_valid1 	= (RoB_retire_in1 & inst1_enable) ? 1 : 0;
+	PRF_free_idx1 		= (RoB_retire_in1 & inst1_enable) ? rrat_reg[RoB_ARF_idx1] : 0;	
 
-		PRF_free_valid2 	= (RoB_retire_in2 & inst2_enable) ? 1 : 0;
-		PRF_free_idx2 	= (RoB_retire_in2 & inst2_enable) ? rrat_reg[RoB_ARF_idx2] : 0;
-		n_rrat_reg[RoB_ARF_idx2]= (RoB_retire_in2 & inst2_enable) ? RoB_PRF_idx2 : rrat_reg[RoB_ARF_idx2];	
+	PRF_free_valid2 	= (RoB_retire_in2 & inst2_enable) ? 1 : 0;
+	PRF_free_idx2 		= (RoB_retire_in2 & inst2_enable) ? rrat_reg[RoB_ARF_idx2] : 0;
 
-	end//else
-end
+	for(int i=0; i<`ARF_SIZE; i++) begin
+		if(i==RoB_ARF_idx1| i==RoB_ARF_idx2) begin
+	    		n_rrat_reg[RoB_ARF_idx1]= (RoB_retire_in1 & inst1_enable) ? RoB_PRF_idx1 : rrat_reg[RoB_ARF_idx1];
+			n_rrat_reg[RoB_ARF_idx2]= (RoB_retire_in2 & inst2_enable) ? RoB_PRF_idx2 : rrat_reg[RoB_ARF_idx2];
+		end
+		else begin
+			n_rrat_reg[i]	= rrat_reg[i];
+		end //else
+	end  //for
+	//$display("rrat_reg[0]:%d", rrat_reg[0]);	
+	//$display("rrat_reg[1]:%d", rrat_reg[1]);	
+	//$display("rrat_reg[2]:%d", rrat_reg[2]);	
+	//$display("rrat_reg[3]:%d", rrat_reg[3]);	
+	//$display("rrat_reg[4]:%d", rrat_reg[4]);	
+
+	//$display("n_rrat_reg[0]:%d", n_rrat_reg[0]);	
+	//$display("n_rrat_reg[1]:%d", n_rrat_reg[1]);	
+	//$display("n_rrat_reg[2]:%d", n_rrat_reg[2]);	
+	//$display("n_rrat_reg[3]:%d", n_rrat_reg[3]);	
+	//$display("n_rrat_reg[4]:%d", n_rrat_reg[4]);
+
+	end //if
 endmodule
