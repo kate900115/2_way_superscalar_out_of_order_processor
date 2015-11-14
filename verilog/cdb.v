17dcbe39d04f8bf49d0c7006a59a3473b3c5a246
Yuxuan Zhang <zyuxuan@umich.edu>
2015-11-13 23:53:16 -0500

    modified:   test_bench/test_cdb.v 	modified:   verilog/cdb.v 	modified:   verilog/cdb_one_entry.v

--- verilog/cdb.v
+++ verilog/cdb.v
@@ -12,52 +12,57 @@
 //////////////////////////////////////////////////////////////////////////
 
 module cdb(
-	input  							adder1_result_ready,
-	input  	[63:0]					adder1_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	adder1_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	adder1_rob_idx,
-	input  							mult1_result_ready,
-	input  	[63:0]					mult1_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	mult1_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	mult1_rob_idx,
-	input  							memory1_result_ready,
-	input  	[63:0]					memory1_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	memory1_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	memory1_rob_idx,
-	input  							adder2_result_ready,
-	input  	[63:0]					adder2_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	adder2_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	adder2_rob_idx,
-	input  							mult2_result_ready,
-	input  	[63:0]					mult2_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	mult2_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	mult2_rob_idx,
-	input  							memory2_result_ready,
-	input  	[63:0]					memory2_result_in,
-	input	[$clog2(`PRF_SIZE)-1:0]	memory2_dest_reg_idx,
-	input	[$clog2(`ROB_SIZE):0]	memory2_rob_idx,
+	input  									adder1_result_ready,
+	input  	[63:0]							adder1_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			adder1_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			adder1_rob_idx,
+	input									adder1_branch_taken,
+	input  									mult1_result_ready,
+	input  	[63:0]							mult1_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			mult1_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			mult1_rob_idx,
+	input  									memory1_result_ready,
+	input  	[63:0]							memory1_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			memory1_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			memory1_rob_idx,
+	input  									adder2_result_ready,
+	input  	[63:0]							adder2_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			adder2_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			adder2_rob_idx,
+	input									adder2_branch_taken,
+	input  									mult2_result_ready,
+	input  	[63:0]							mult2_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			mult2_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			mult2_rob_idx,
+	input  									memory2_result_ready,
+	input  	[63:0]							memory2_result_in,
+	input	[$clog2(`PRF_SIZE)-1:0]			memory2_dest_reg_idx,
+	input	[$clog2(`ROB_SIZE):0]			memory2_rob_idx,
 	
 	output 	logic							cdb1_valid,
 	output  logic [$clog2(`PRF_SIZE)-1:0]	cdb1_tag,
 	output 	logic [63:0]					cdb1_out,
 	output  logic [$clog2(`ROB_SIZE):0]		cdb1_rob_idx,
+	output  logic							cdb1_branch_is_taken,
 	output 	logic							cdb2_valid,
 	output  logic [$clog2(`PRF_SIZE)-1:0]	cdb2_tag,
 	output 	logic [63:0]					cdb2_out,
 	output  logic [$clog2(`ROB_SIZE):0]		cdb2_rob_idx,
-	output 	logic				adder1_send_in_success,
-	output 	logic				adder2_send_in_success,
-	output 	logic				mult1_send_in_success,
-	output 	logic				mult2_send_in_success,
-	output 	logic				memory1_send_in_success,
-	output 	logic				memory2_send_in_success
+	output  logic							cdb2_branch_is_taken,
+	output 	logic							adder1_send_in_success,
+	output 	logic							adder2_send_in_success,
+	output 	logic							mult1_send_in_success,
+	output 	logic							mult2_send_in_success,
+	output 	logic							memory1_send_in_success,
+	output 	logic							memory2_send_in_success
+
 );
 
-	logic	[5:0]				fu_result_ready;
-	logic	[5:0]				fu_send_in_success;
-	logic   [5:0]				fu_result_ready2;
-	logic   [5:0]				fu_select1;
-	logic   [5:0]				fu_select2;
+	logic	[5:0]							fu_result_ready;
+	logic	[5:0]							fu_send_in_success;
+	logic   [5:0]							fu_result_ready2;
+	logic   [5:0]							fu_select1;
+	logic   [5:0]							fu_select2;
 
 
 	assign  memory1_send_in_success = fu_send_in_success[5];
@@ -68,7 +73,7 @@ module cdb(
 	assign  adder2_send_in_success  = fu_send_in_success[0];
 
 	assign  fu_result_ready      = {memory1_result_ready, memory2_result_ready, mult1_result_ready,
-				        mult2_result_ready, adder1_result_ready, adder2_result_ready};
+				        			mult2_result_ready, adder1_result_ready, adder2_result_ready};
 
 	assign  fu_result_ready2     = (~fu_select1) & fu_result_ready;	
 	
@@ -80,6 +85,7 @@ module cdb(
 		.adder1_result_in(adder1_result_in),
 		.adder1_dest_reg_idx(adder1_dest_reg_idx),
 		.adder1_rob_idx(adder1_rob_idx),
+		.adder1_branch_taken(adder1_branch_taken),
 		.mult1_result_in(mult1_result_in),
 		.mult1_dest_reg_idx(mult1_dest_reg_idx),
 		.mult1_rob_idx(mult1_rob_idx),
@@ -89,6 +95,7 @@ module cdb(
 		.adder2_result_in(adder2_result_in),
 		.adder2_dest_reg_idx(adder2_dest_reg_idx),
 		.adder2_rob_idx(adder2_rob_idx),
+		.adder2_branch_taken(adder2_branch_taken),
 		.mult2_result_in(mult2_result_in),
 		.mult2_dest_reg_idx(mult2_dest_reg_idx),
 		.mult2_rob_idx(mult2_rob_idx),
@@ -100,7 +107,8 @@ module cdb(
 		.cdb_valid(cdb1_valid),
 		.cdb_tag(cdb1_tag),
 		.cdb_out(cdb1_out),
-		.cdb_rob_idx(cdb1_rob_idx)
+		.cdb_rob_idx(cdb1_rob_idx),
+		.branch_is_taken(cdb1_branch_is_taken)
 	);
 
 	cdb_one_entry cdb2(
@@ -109,6 +117,7 @@ module cdb(
 		.adder1_result_in(adder1_result_in),
 		.adder1_dest_reg_idx(adder1_dest_reg_idx),
 		.adder1_rob_idx(adder1_rob_idx),
+		.adder1_branch_taken(adder1_branch_taken),
 		.mult1_result_in(mult1_result_in),
 		.mult1_dest_reg_idx(mult1_dest_reg_idx),
 		.mult1_rob_idx(mult1_rob_idx),
@@ -118,6 +127,7 @@ module cdb(
 		.adder2_result_in(adder2_result_in),
 		.adder2_dest_reg_idx(adder2_dest_reg_idx),
 		.adder2_rob_idx(adder2_rob_idx),
+		.adder2_branch_taken(adder2_branch_taken),
 		.mult2_result_in(mult2_result_in),
 		.mult2_dest_reg_idx(mult2_dest_reg_idx),
 		.mult2_rob_idx(mult2_rob_idx),
@@ -129,7 +139,8 @@ module cdb(
 		.cdb_valid(cdb2_valid),
 		.cdb_tag(cdb2_tag),
 		.cdb_out(cdb2_out),
-		.cdb_rob_idx(cdb2_rob_idx)
+		.cdb_rob_idx(cdb2_rob_idx),
+		.branch_is_taken(cdb2_branch_is_taken)
 	);
 
 	priority_selector #(.WIDTH(6)) cdb_psl1( 
--- verilog/cdb_one_entry.v
+++ verilog/cdb_one_entry.v
@@ -33,15 +33,18 @@ module cdb_one_entry(
 	input  	[63:0]							adder1_result_in,
 	input	[$clog2(`PRF_SIZE)-1:0]			adder1_dest_reg_idx,
 	input	[$clog2(`ROB_SIZE):0]			adder1_rob_idx,
+	input									adder1_branch_taken,
 	input  	[63:0]							adder2_result_in,
 	input	[$clog2(`PRF_SIZE)-1:0]			adder2_dest_reg_idx,
 	input	[$clog2(`ROB_SIZE):0]			adder2_rob_idx,
+	input									adder2_branch_taken,
 
 	
 	output 	logic							cdb_valid,
 	output  logic [$clog2(`PRF_SIZE)-1:0]	cdb_tag,
 	output 	logic [63:0]					cdb_out,
-	output  [$clog2(`ROB_SIZE):0]			cdb_rob_idx
+	output  logic [$clog2(`ROB_SIZE):0]		cdb_rob_idx,
+	output	logic							branch_is_taken
 );
 
 	
@@ -55,6 +58,7 @@ module cdb_one_entry(
 				cdb_tag   		   = memory1_dest_reg_idx;
 				cdb_out   		   = memory1_result_in;
 				cdb_rob_idx		   = memory1_rob_idx;
+				branch_is_taken	   = 0;
 			end
 		6'b010000:
 			begin
@@ -62,6 +66,7 @@ module cdb_one_entry(
 				cdb_tag   		   = memory2_dest_reg_idx;
 				cdb_out   		   = memory2_result_in;
 				cdb_rob_idx		   = memory2_rob_idx;
+				branch_is_taken	   = 0;
 			end
 		6'b001000:
 			begin
@@ -69,6 +74,7 @@ module cdb_one_entry(
 				cdb_tag   		   = mult1_dest_reg_idx;
 				cdb_out   		   = mult1_result_in;
 				cdb_rob_idx		   = mult1_rob_idx;
+				branch_is_taken	   = 0;
 			end
 		6'b000100:
 			begin
@@ -76,6 +82,7 @@ module cdb_one_entry(
 				cdb_tag   		   = mult2_dest_reg_idx;
 				cdb_out   		   = mult2_result_in;
 				cdb_rob_idx		   = mult2_rob_idx;
+				branch_is_taken	   = 0;
 			end
 		6'b000010:
 			begin
@@ -83,6 +90,7 @@ module cdb_one_entry(
 				cdb_tag  		   = adder1_dest_reg_idx;
 				cdb_out  		   = adder1_result_in;
 				cdb_rob_idx		   = adder1_rob_idx;
+				branch_is_taken	   = adder1_branch_taken;
 				
 			end
 		6'b000001:
@@ -91,6 +99,7 @@ module cdb_one_entry(
 				cdb_tag   		   = adder2_dest_reg_idx;
 				cdb_out   		   = adder2_result_in;
 				cdb_rob_idx		   = adder2_rob_idx;
+				branch_is_taken	   = adder2_branch_taken;
 			end
 		default:
 			begin
@@ -98,6 +107,7 @@ module cdb_one_entry(
 				cdb_tag   		   = 0;
 				cdb_out   		   = 0;
 				cdb_rob_idx		   = 0;
+				branch_is_taken	   = 0;
 			end
 		endcase
 	end
