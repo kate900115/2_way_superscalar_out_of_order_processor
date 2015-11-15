db694624319ab154128f2fc26704cf7a4473a24d
Qian Shen <shenqian@umich.edu>
2015-11-14 20:35:25 -0500

    update test

--- test_bench/test_id_stage.v
+++ test_bench/test_id_stage.v
@@ -28,25 +28,25 @@ module test_id_stage;
 
 	logic        id_rd_mem_out1;        // does inst read memory?
 	logic        id_wr_mem_out1;        // does inst write memory?
-	logic        id_ldl_mem_out1;       // load-lock inst?
-	logic        id_stc_mem_out1;       // store-conditional inst?
+	//logic        id_ldl_mem_out1;       // load-lock inst?
+	//logic        id_stc_mem_out1;       // store-conditional inst?
 	logic        id_cond_branch_out1;   // is inst a conditional branch?
 	logic        id_uncond_branch_out1; // is inst an unconditional branch 
 													        // or jump?
 	logic        id_halt_out1;
-	logic        id_cpuid_out1;         // get CPUID inst?
+	//logic        id_cpuid_out1;         // get CPUID inst?
 	logic        id_illegal_out1;
 	logic        id_valid_inst_out1;     // is inst a valid instruction to be 
 									        // counted for CPI calculations?
 	logic        id_rd_mem_out2;        // does inst read memory?
 	logic        id_wr_mem_out2;        // does inst write memory?
-	logic        id_ldl_mem_out2;       // load-lock inst?
-	logic        id_stc_mem_out2;       // store-conditional inst?
+	//logic        id_ldl_mem_out2;       // load-lock inst?
+	//logic        id_stc_mem_out2;       // store-conditional inst?
 	logic        id_cond_branch_out2;   // is inst a conditional branch?
 	logic        id_uncond_branch_out2; // is inst an unconditional branch 
 											        		// or jump?
 	logic        id_halt_out2;
-	logic        id_cpuid_out2;         // get CPUID inst?
+	//logic        id_cpuid_out2;         // get CPUID inst?
 	logic        id_illegal_out2;
 	logic        id_valid_inst_out2;     // is inst a valid instruction to be 
 
@@ -79,25 +79,25 @@ module test_id_stage;
 
 			.id_rd_mem_out1(id_rd_mem_out1),        // does inst read memory?
 			.id_wr_mem_out1(id_wr_mem_out1),        // does inst write memory?
-			.id_ldl_mem_out1(id_ldl_mem_out1),       // load-lock inst?
-			.id_stc_mem_out1(id_stc_mem_out1),       // store-conditional inst?
+			//.id_ldl_mem_out1(id_ldl_mem_out1),       // load-lock inst?
+			//.id_stc_mem_out1(id_stc_mem_out1),       // store-conditional inst?
 			.id_cond_branch_out1(id_cond_branch_out1),   // is inst a conditional branch?
 			.id_uncond_branch_out1(id_uncond_branch_out1), // is inst an unconditional branch 
 													        // or jump?
 			.id_halt_out1(id_halt_out1),
-			.id_cpuid_out1(id_cpuid_out1),         // get CPUID inst?
+			//.id_cpuid_out1(id_cpuid_out1),         // get CPUID inst?
 			.id_illegal_out1(id_illegal_out1),
 			.id_valid_inst_out1(id_valid_inst_out1),     // is inst a valid instruction to be 
 									        // counted for CPI calculations?
 			.id_rd_mem_out2(id_rd_mem_out2),        // does inst read memory?
 			.id_wr_mem_out2(id_wr_mem_out2),        // does inst write memory?
-			.id_ldl_mem_out2(id_ldl_mem_out2),       // load-lock inst?
-			.id_stc_mem_out2(id_stc_mem_out2),       // store-conditional inst?
+			//.id_ldl_mem_out2(id_ldl_mem_out2),       // load-lock inst?
+			//.id_stc_mem_out2(id_stc_mem_out2),       // store-conditional inst?
 			.id_cond_branch_out2(id_cond_branch_out2),   // is inst a conditional branch?
 			.id_uncond_branch_out2(id_uncond_branch_out2), // is inst an unconditional branch 
 											        		// or jump?
 			.id_halt_out2(id_halt_out2),
-			.id_cpuid_out2(id_cpuid_out2),         // get CPUID inst?
+			//.id_cpuid_out2(id_cpuid_out2),         // get CPUID inst?
 			.id_illegal_out2(id_illegal_out2),
 			.id_valid_inst_out2(id_valid_inst_out2)     // is inst a valid instruction to be
 	);
@@ -114,17 +114,17 @@ module test_id_stage;
 	
 	initial begin
 		$monitor("@@@time:%.0f, clk:%b, opa_mux_out1:%h, opb_mux_out1:%h, opa_mux_tag1:%b, opb_mux_tag1:%b, id_dest_reg_idx_out1:%b, \
-			     id_alu_func_out1:%b, id_op_type_inst1:%b, id_rd_mem_out1:%b, id_wr_mem_out1:%b, id_ldl_mem_out1:%b, id_stc_mem_out1:%b,\
-			     id_cond_branch_out1:%b, id_uncond_branch_out1:%b, id_halt_out1:%b, id_cpuid_out1:%b, id_illegal_out1:%b, id_valid_inst_out1:%b, \
+			     id_alu_func_out1:%b, id_op_type_inst1:%b, id_rd_mem_out1:%b, id_wr_mem_out1:%b, \
+			     id_cond_branch_out1:%b, id_uncond_branch_out1:%b, id_halt_out1:%b, id_illegal_out1:%b, id_valid_inst_out1:%b, \
 			     opa_mux_out2:%h, opb_mux_out2:%h, opa_mux_tag2:%b, opb_mux_tag2:%b, id_dest_reg_idx_out2:%b,  id_alu_func_out2:%b, \
-			     id_op_type_inst2:%b, id_rd_mem_out2:%b, id_wr_mem_out2:%b, id_ldl_mem_out2:%b, id_stc_mem_out2:%b, id_cond_branch_out2:%b, \
-			     id_uncond_branch_out2:%b, id_halt_out2:%b, id_cpuid_out2:%b, id_illegal_out2:%b, id_valid_inst_out2:%b", 
+			     id_op_type_inst2:%b, id_rd_mem_out2:%b, id_wr_mem_out2:%b, id_cond_branch_out2:%b, \
+			     id_uncond_branch_out2:%b, id_halt_out2:%b, id_illegal_out2:%b, id_valid_inst_out2:%b", 
 			   $time, clock, opa_mux_out1, opb_mux_out1, opa_mux_tag1, opb_mux_tag1, id_dest_reg_idx_out1,
-			     id_alu_func_out1, id_op_type_inst1, id_rd_mem_out1, id_wr_mem_out1, id_ldl_mem_out1, id_stc_mem_out1,
-			     id_cond_branch_out1, id_uncond_branch_out1, id_halt_out1, id_cpuid_out1, id_illegal_out1, id_valid_inst_out1, 
+			     id_alu_func_out1, id_op_type_inst1, id_rd_mem_out1, id_wr_mem_out1,
+			     id_cond_branch_out1, id_uncond_branch_out1, id_halt_out1, id_illegal_out1, id_valid_inst_out1, 
 			     opa_mux_out2, opb_mux_out2, opa_mux_tag2, opb_mux_tag2, id_dest_reg_idx_out2,  id_alu_func_out2,
-			     id_op_type_inst2, id_rd_mem_out2, id_wr_mem_out2, id_ldl_mem_out2, id_stc_mem_out2, id_cond_branch_out2, 
-			     id_uncond_branch_out2, id_halt_out2, id_cpuid_out2, id_illegal_out2, id_valid_inst_out2 );
+			     id_op_type_inst2, id_rd_mem_out2, id_wr_mem_out2, id_cond_branch_out2, 
+			     id_uncond_branch_out2, id_halt_out2, id_illegal_out2, id_valid_inst_out2 );
 			
 		clock = 0;
 		//***RESET**
