67fd8d51ae2518e540c0f84616986725392ad386
Qian Shen <shenqian@umich.edu>
2015-12-04 00:44:33 -0500

    btb

--- verilog/BTB.v
+++ verilog/BTB.v
@@ -13,10 +13,10 @@ module BTB(
 		input target_pc1_valid,
 		input target_pc2_valid,
 		
-		output [63:0] target_inst1_pc,
-		output [63:0] target_inst2_pc,
-		output target_inst1_valid,
-		output target_inst2_valid
+		output logic [63:0] target_inst1_pc,
+		output logic [63:0] target_inst2_pc,
+		output logic target_inst1_valid,
+		output logic target_inst2_valid
 	);
 	
 	logic [`BTB_SIZE-1:0] [63:0] BTB_pc_1;
@@ -33,12 +33,20 @@ module BTB(
 	logic [`BTB_SIZE-1:0] next_valid_2;
 	logic [`BTB_SIZE-1:0] switch;        //0 means pointing to way1,1 means pointing to way2
 	logic [`BTB_SIZE-1:0] next_switch;
+	//logic [`BTB_SIZE-1:0] next_switch_hit;
 	
 	always_comb begin
-		for(i=0; i<`BTB_SIZE; i++) begin
+		for(int i=0; i<`BTB_SIZE; i++) begin
 			BTB_nextpc_1[i]=BTB_pc_1[i];
 			BTB_nextidx1[i]=BTB_idx1[i];
 			next_valid_1[i]=valid_1[i];
+			BTB_nextpc_2[i]=BTB_pc_2[i];
+			BTB_nextidx2[i]=BTB_idx2[i];
+			next_valid_2[i]=valid_2[i];
+			//target_inst1_pc=0;
+			//target_inst1_valid=1'b0;
+			//target_inst2_pc=0;
+			//target_inst2_valid=1'b0;
 			next_switch[i] =switch;
 			if(i==pc_idx1[$clog2(`BTB_SIZE)+1:2] && target_pc1_valid) begin       //index match
 				if(~valid_1[i] && ~valid_2[i]) begin                                       //if has vacancy
@@ -72,90 +80,160 @@ module BTB(
 						next_valid_1[i]=1'b1;
 						next_switch[i] =1'b1;
 					end
-				//end
+				end
 			end
 			
 			if(i==pc_idx2[$clog2(`BTB_SIZE)+1:2] && target_pc2_valid) begin
 				if(~valid_1[i] && ~valid_2[i]) begin
-					BTB_nextpc_1[i]=target_pc1;
-					BTB_nextidx1[i]=pc_idx1[63:$clog2(`BTB_SIZE)+2];
+					BTB_nextpc_1[i]=target_pc2;
+					BTB_nextidx1[i]=pc_idx2[63:$clog2(`BTB_SIZE)+2];
 					next_valid_1[i]=1'b1;
 					next_switch[i] =1'b1;
 				end
 				else if(~valid_1[i]) begin
-					BTB_nextpc_1[i]=target_pc1;
-					BTB_nextidx1[i]=pc_idx1[63:$clog2(`BTB_SIZE)+2];
+					BTB_nextpc_1[i]=target_pc2;
+					BTB_nextidx1[i]=pc_idx2[63:$clog2(`BTB_SIZE)+2];
 					next_valid_1[i]=1'b1;
 					next_switch[i] =1'b1;
 				end
 				else if(~valid_2[i]) begin
-					BTB_nextpc_2[i]=target_pc1;
-					BTB_nextidx2[i]=pc_idx1[63:$clog2(`BTB_SIZE)+2];
+					BTB_nextpc_2[i]=target_pc2;
+					BTB_nextidx2[i]=pc_idx2[63:$clog2(`BTB_SIZE)+2];
 					next_valid_2[i]=1'b1;
 					next_switch[i] =1'b0;
 				end
 				else begin
 					if(switch[i]) begin
-						BTB_nextpc_2[i]=target_pc1;
-						BTB_nextidx2[i]=pc_idx1[63:$clog2(`BTB_SIZE)+2];
+						BTB_nextpc_2[i]=target_pc2;
+						BTB_nextidx2[i]=pc_idx2[63:$clog2(`BTB_SIZE)+2];
 						next_valid_2[i]=1'b1;
 						next_switch[i] =1'b0;
 					end
 					else begin
-						BTB_nextpc_1[i]=target_pc1;
-						BTB_nextidx1[i]=pc_idx1[63:$clog2(`BTB_SIZE)+2];
+						BTB_nextpc_1[i]=target_pc2;
+						BTB_nextidx1[i]=pc_idx2[63:$clog2(`BTB_SIZE)+2];
 						next_valid_1[i]=1'b1;
 						next_switch[i] =1'b1;
 					end
 				end
 			end
+
+			if(inst1_valid && i==if_inst1_pc[$clog2(`BTB_SIZE)+1:2]) begin
+				if(valid_1[i] && BTB_idx1[i]==if_inst1_pc[63:$clog2(`BTB_SIZE)+2]) begin
+					//target_inst1_pc=BTB_pc_1[i];
+					//target_inst1_valid=1'b1;
+					//next_switch_hit[i]=1'b1;
+					next_switch[i]=1'b1;
+				end
+				else if(valid_2[i] && BTB_idx2[i]==if_inst1_pc[63:$clog2(`BTB_SIZE)+2]) begin
+					//target_inst1_pc=BTB_pc_2[i];
+					//target_inst1_valid=1'b1;
+					//next_switch_hit[i]=1'b0;
+					next_switch[i]=1'b0;
+				end
+			end
+			
+			if(inst2_valid && i==if_inst2_pc[$clog2(`BTB_SIZE)+1:2]) begin
+				if(valid_1[i] && BTB_idx1[i]==if_inst2_pc[63:$clog2(`BTB_SIZE)+2]) begin
+					//target_inst2_pc=BTB_pc_1[i];
+					//target_inst2_valid=1'b1;
+					//next_switch_hit[i]=1'b1;
+					next_switch[i]=1'b1;
+				end
+				else if(valid_2[i] && BTB_idx2[i]==if_inst2_pc[63:$clog2(`BTB_SIZE)+2]) begin
+					//target_inst2_pc=BTB_pc_2[i];
+					//target_inst2_valid=1'b1;
+					//next_switch_hit[i]=1'b0;
+					next_switch[i]=1'b0;
+				end
+			end
 		end
 	end
 	
-	always_comb begin
-		for (int i=0; i< `BTB_SIZE; i++) begin
+	/*always_comb begin
+		for(int i=0; i<`BTB_SIZE; i++) begin
 			target_inst1_pc=0;
 			target_inst1_valid=1'b0;
 			target_inst2_pc=0;
 			target_inst2_valid=1'b0;
-			next_switch[i]=switch[i];
+			//next_switch_hit[i]=switch[i];
 			if(inst1_valid && i==if_inst1_pc[$clog2(`BTB_SIZE)+1:2]) begin
 				if(valid_1[i] && if_inst1_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx1[i]) begin
 					target_inst1_pc=BTB_pc_1[i];
 					target_inst1_valid=1'b1;
-					next_switch[i]=1'b1;
+					//next_switch_hit[i]=1'b1;
 				end
 				else if(valid_2[i] && if_inst1_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx2[i]) begin
 					target_inst1_pc=BTB_pc_2[i];
 					target_inst1_valid=1'b1;
-					next_switch[i]=1'b0;
+					//next_switch_hit[i]=1'b0;
 				end
-				/*else begin
+				else begin
 					target_inst1_pc=0;
 					target_inst1_valid=1'b0;
 					next_switch[i]=switch[i];
-				end*/
+				end
 			end
 			
 			if(inst2_valid && i==if_inst2_pc[$clog2(`BTB_SIZE)+1:2]) begin
 				if(valid_1[i] && if_inst2_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx1[i]) begin
 					target_inst2_pc=BTB_pc_1[i];
 					target_inst2_valid=1'b1;
-					next_switch[i]=1'b1;
+					//next_switch_hit[i]=1'b1;
 				end
 				else if(valid_2[i] && if_inst2_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx2[i]) begin
 					target_inst2_pc=BTB_pc_2[i];
 					target_inst2_valid=1'b1;
-					next_switch[i]=1'b0;
+					//next_switch_hit[i]=1'b0;
 				end
-				/*else begin
+				else begin
 					target_inst2_pc=0;
 					target_inst2_valid=1'b0;
 					next_switch[i]=switch[i];
-				end*/
+				end
 			end
 		end
-	end
+	end*/
+
+	//assign target_inst1_pc = inst1_valid ? BTB_idx1[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]]==if_inst1_pc[63:$clog2(`BTB_SIZE)+2] &&
+	
+		always_comb begin
+				if(inst1_valid) begin
+					if(valid_1[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]] && if_inst1_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx1[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]]) begin
+						target_inst1_pc=BTB_pc_1[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]];
+						target_inst1_valid=1'b1;
+						//next_switch_hit[i]=1'b1;
+					end
+					else if(valid_2[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]] && if_inst1_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx2[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]]) begin
+						target_inst1_pc=BTB_pc_2[if_inst1_pc[$clog2(`BTB_SIZE)+1:2]];
+						target_inst1_valid=1'b1;
+						//next_switch_hit[i]=1'b0;
+					end
+					else begin
+						target_inst1_pc=0;
+						target_inst1_valid=1'b0;
+						//next_switch[i]=switch[i];
+					end
+				end
+			
+				if(inst2_valid) begin
+					if(valid_1[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]] && if_inst2_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx1[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]]) begin
+						target_inst2_pc=BTB_pc_1[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]];
+						target_inst2_valid=1'b1;
+						//next_switch_hit[i]=1'b1;
+					end
+					else if(valid_2[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]] && if_inst2_pc[63:$clog2(`BTB_SIZE)+2] == BTB_idx2[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]]) begin
+						target_inst2_pc=BTB_pc_2[if_inst2_pc[$clog2(`BTB_SIZE)+1:2]];
+						target_inst2_valid=1'b1;
+						//next_switch_hit[i]=1'b0;
+					end
+					else begin
+						target_inst2_pc=0;
+						target_inst2_valid=1'b0;
+						//next_switch[i]=switch[i];
+					end
+				end
+		end
 	
 	always_ff @(posedge clock) begin
 		if(reset) begin
@@ -175,6 +253,7 @@ module BTB(
 			BTB_idx2 <= `SD BTB_nextidx2;
 			valid_1  <= `SD next_valid_1;
 			valid_2  <= `SD next_valid_2;
+			//if((inst1_valid || inst2_valid) && (target_inst1_valid || target_inst2_valid))
 			switch   <= `SD next_switch;
 		end
 	end
