9e6ab68e367f4896c31de6ff91459034fe9a82ad
gaoying <gaoying@umich.edu>
2015-12-08 00:19:25 -0500

    modified icache

--- verilog/icache.v
+++ verilog/icache.v
@@ -1,15 +1,22 @@
-module icache(
-	input							clock,
-	input							reset,
-	input thread1_branch_is_taken,
+module icache_controller(
+	// input from Mem.v										
+	input	[3:0]							Imem2proc_response,   //
+	input	[3:0]							Imem2proc_tag,
+	
 	// input from processor.v
 	input	[63:0]							proc2Icache_addr,	
 	input  BUS_COMMAND						proc2Icache_command,
 	
-	// input from memory
-	input	[3:0]							Imem2proc_response,
-	input	[3:0]							Imem2proc_tag,
-	input	[`ICACHE_BLOCK_SIZE-1:0]		Imem2proc_data,
+	//input from prefetch
+	input	[63:0]							pref2Icache_addr,	
+	input  BUS_COMMAND						pref2Icache_command,
+	
+	// input from Icache.v
+	input  [`ICACHE_BLOCK_SIZE-1:0]			cachemem_data,
+	input 									cachemem_valid,
+	input									cachemem_is_full,
+	input									cachemem_is_miss,
+	input									cachemem_is_miss_pref,	
 	
 	// output to mem.v
 	output	BUS_COMMAND						proc2Imem_command,
@@ -17,76 +24,80 @@ module icache(
 	
 	// output to processor.v
 	output	logic	[63:0]					Icache_data_out,
-	output	logic							Icache_valid_out,
-	output  logic [3:0]						Icache2proc_tag,	 	
-	output  logic [3:0]						Icache2proc_response
+	output	logic							Icache_data_valid,
 	
-	);
+	// output to Icache.v
+	output logic [`ICACHE_INDEX_SIZE-1:0]   index_pref,
+	output logic [`ICACHE_TAG_SIZE-1:0]		tag_pref,  
+	output logic							read_enable_pref,    
+	output logic [3:0]						mem_response,   //when prefecth stores data
 	
-	// input from Icachemem.v
-	logic  [`ICACHE_BLOCK_SIZE-1:0]			cachemem_data;
-	logic 									cachemem_valid;
-	logic									cachemem_is_full;
-	logic									cachemem_is_miss;	
-	// output to Icachemem.v
-	logic [`ICACHE_INDEX_SIZE-1:0]  		index;
-	logic [`ICACHE_TAG_SIZE-1:0]			tag; 
-	logic									read_enable;    
-	logic [3:0]								mem_response;
-	logic [3:0]								mem_tag;
-	logic Icache_valid_out1;
-	logic load_valid;
-	icache_controller ic(
-		// input from Mem.v									
-		.Imem2proc_response(Imem2proc_response),
-		.Imem2proc_tag(Imem2proc_tag),
-		// input from processor.v
-		.proc2Icache_addr(proc2Icache_addr),	
-		.proc2Icache_command(proc2Icache_command),
-		// input from Icache.v
-		.cachemem_data(cachemem_data),
-		.cachemem_valid(cachemem_valid),
-		.cachemem_is_full(cachemem_is_full),
-		.cachemem_is_miss(cachemem_is_miss),
-	
-		// output to mem.v
-		.proc2Imem_command(proc2Imem_command),
-		.proc2Imem_addr(proc2Imem_addr),
-		// output to processor.v
-		.Icache_data_out(Icache_data_out),
-		.Icache_data_valid(Icache_valid_out1),
-		.Icache2proc_tag(Icache2proc_tag),	 	
-		.Icache2proc_response(Icache2proc_response),
 		// output to Icache.v
-		.index(index),
-		.tag(tag),  
-		.read_enable(read_enable),    
-		.mem_response(mem_response),
-		.mem_tag(mem_tag)
+	output logic [`ICACHE_INDEX_SIZE-1:0]   index,
+	output logic [`ICACHE_TAG_SIZE-1:0]		tag,  
+	output logic							read_enable,    
+	output logic [3:0]						mem_tag            // when inst look for data
 );
-assign Icache_valid_out = Icache_valid_out1 || load_valid;
+	assign 	read_enable 		    	 = (proc2Icache_command==BUS_LOAD);
+	assign 	read_enable_pref 	    	 = (pref2Icache_command==BUS_LOAD);
+	// output to Icache.v
+	assign {tag, index} 			= proc2Icache_addr[63:`ICACHE_BLOCK_OFFSET];
+	assign {tag_pref, index_pref} 	= pref2Icache_addr[63:`ICACHE_BLOCK_OFFSET];
+	assign mem_tag			  		= Imem2proc_tag;
+	//only tag, index, read_enable are needed to get miss
 	
-	
-	icachemem im(
-		.clock(clock),
-		.reset(reset|| thread1_branch_is_taken),
-		// input from icache_controller.v
-		.index_in(index),
-		.tag_in(tag),
-		.read_enable(read_enable),
-		.mem_response(mem_response),
-		.mem_tag(mem_tag),						
-	
-		// input from mem.v
-		.load_data_in(Imem2proc_data),
-	
-		// output to icache_controller.v
-		.data_is_valid(cachemem_valid),
-		.data_is_miss(cachemem_is_miss),
-		.cache_is_full(cachemem_is_full),
-		.read_data(cachemem_data),
-		.load_valid(load_valid)
-	);
-
+	//input to mem.v
+	always_comb begin
+		case(pref2Icache_command)
+			BUS_LOAD:
+				begin
+					// to icache.v
+					if (cachemem_is_miss_pref && cachemem_is_full) 
+					begin
+						proc2Imem_command 		 = BUS_NONE;
+						proc2Imem_addr 	 		 = 0;
+						mem_response	  		 = 0;
+					end
+					else if (cachemem_is_miss_pref && !cachemem_is_full)
+					begin
+						proc2Imem_command 		 = BUS_LOAD;
+						proc2Imem_addr 	 		 = {pref2Icache_addr[63:3],3'b0};
+						mem_response	 		 = Imem2proc_response;
+					end
+					else if //((cachemem_is_miss==0) && (Imem2proc_tag!=0))
+					(!cachemem_is_miss_pref)   //HIT!!!
+					begin
+						proc2Imem_command		 = BUS_NONE;
+						proc2Imem_addr 	 		 = 0;
+						mem_response			 = Imem2proc_response;
+					end
+				end
+			
+			BUS_NONE:
+				begin
+						// to mem.v
+						proc2Imem_command		 = BUS_NONE;
+						proc2Imem_addr 	 		 = 0;
+						// to icache.v
+						mem_response			 = 0;
+				end
+				
+			default:
+				begin
+					// to mem.v
+					proc2Imem_command		 = BUS_NONE;
+					proc2Imem_addr 	 		 = 0;
+					// to icache.v
+					mem_response			 = 0;
+				end
+		endcase
+	end	
+			
+//output to processor	
+	always_comb
+	begin
+		Icache_data_valid		 = cachemem_valid;
+		Icache_data_out  		 = cachemem_data;
+	end	
 endmodule
 
